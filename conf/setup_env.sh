d734a33d26ff304488222e60136914b232fcfb4f
rubnsbarbosa <rubensbarbosa90@gmail.com>
2024-06-30 18:24:10 +0200

    config files added

---
+++ conf/setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export NASA_DATASET_PATH="/<PATH>/nasa-asteroids-extractor/nasa_asteroids_data"
+export GOOGLE_APPLICATION_CREDENTIALS="config/service_account_example.json"
+export GOOGLE_CLOUD_PROJECT="de-ru-data-engineering"
+export GOOGLE_CLOUD_SECRET="nasa-api-key"
+export GOOGLE_CLOUD_BUCKET="de-bu-nasa-asteroids"
+export GOOGLE_BQ_TABLE_ID="de-ru-data-engineering.asteroids.near_asteroids"
